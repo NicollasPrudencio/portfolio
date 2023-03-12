f502435589df25b1a6015c9da9cd19266b948369
Nicollas Prudencio <67238467+nic37@users.noreply.github.com>
2023-03-12 15:20:32 -0300

    medias não estão sendo aplicadas. Foram escritas no escopo global.

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Sidebar from './containers/Sidebar'
 import reportWebVitals from './reportWebVitals'
-import Teste from './Teste'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <Teste />
+    {/* <Sidebar /> */}
+    <App />
   </React.StrictMode>
 )
 
