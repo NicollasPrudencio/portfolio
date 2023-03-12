4409e423ac48af60310a5c5d74bcfab97235a74a
Nicollas Prudencio <67238467+nic37@users.noreply.github.com>
2023-03-12 20:35:05 -0300

    Estilizando a Sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,10 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => (
+  <FotoPerfil
+    src="https://github.com/nic37.png"
+    alt="Foto de perfil do Github"
+  />
+)
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/nic37.png" />
-    <Titulo fontSize={20}>Nicollas Prudencio</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Nicollas Prudencio</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        nic37
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
